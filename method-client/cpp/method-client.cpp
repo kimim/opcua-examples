07d09b5c6a7ed8c3da49a2a65c7adaa92a571336
Kimi Ma <kimi.im@outlook.com>
2024-07-12 12:10:21 +0800

    add method

---
+++ method-client/cpp/method-client.cpp
@@ -0,0 +1,17 @@
+#include "open62541pp/open62541pp.h"
+
+int main() {
+    opcua::Client client;
+    client.connect("opc.tcp://localhost:4840");
+
+    // Browse method node
+    auto objNode = client.getObjectsNode();
+    auto greetMethodNode = objNode.browseChild({{1, "Greet"}});
+
+    // Call method from parent node (Objects)
+    const auto outputs = objNode.callMethod(
+        greetMethodNode.id(), {opcua::Variant::fromScalar("World")}
+    );
+    log(client, opcua::LogLevel::Info, opcua::LogCategory::Client,
+        outputs.at(0).getScalar<opcua::String>());
+}
