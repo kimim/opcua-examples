566beb44c37dc38f2347d31b0e8bdda564f332bb
Kimi Ma <kimi.im@outlook.com>
2025-12-31 15:41:21 +0800

    set applicationName, applicationUri, and productUri

--- variable_server/c/variable-server.c
+++ variable_server/c/variable-server.c
@@ -1,28 +1,39 @@
+#include <open62541/server_config_default.h>
 #include <open62541/server.h>
 
 static void
 addVariable(UA_Server *server) {
     UA_VariableAttributes attr = UA_VariableAttributes_default;
-    UA_Int32 answer = 42;
-    UA_Variant_setScalar(&attr.value, &answer, &UA_TYPES[UA_TYPES_INT32]);
-    attr.description = UA_LOCALIZEDTEXT("en-US","The Answer");
     attr.displayName = UA_LOCALIZEDTEXT("en-US","The Answer");
+    attr.description = UA_LOCALIZEDTEXT("en-US","Answer to the Ultimate Question of Life");
     attr.dataType = UA_TYPES[UA_TYPES_INT32].typeId;
     attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    UA_Int32 answer = 42;
+    UA_Variant_setScalar(&attr.value, &answer, &UA_TYPES[UA_TYPES_INT32]);
 
     /* Add the variable node to the information model */
     UA_NodeId answerNodeId = UA_NODEID_STRING(1, "answer");
-    UA_QualifiedName answerName = UA_QUALIFIEDNAME(1, "TheAnswer");
+    UA_QualifiedName answerBrowseName = UA_QUALIFIEDNAME(1, "TheAnswer");
     UA_NodeId parentNodeId = UA_NS0ID(OBJECTSFOLDER);
     UA_NodeId parentReferenceNodeId = UA_NS0ID(ORGANIZES);
     UA_Server_addVariableNode(server, answerNodeId, parentNodeId,
-                              parentReferenceNodeId, answerName,
+                              parentReferenceNodeId, answerBrowseName,
                               UA_NS0ID(BASEDATAVARIABLETYPE), attr, NULL, NULL);
 }
 
 
 int main(void) {
-    UA_Server *server = UA_Server_new();
+    UA_ServerConfig config;
+    memset(&config, 0, sizeof(UA_ServerConfig));
+    UA_ServerConfig_setDefault(&config);
+    config.applicationDescription.applicationName =
+        UA_LOCALIZEDTEXT("en-US", "open62541 variable server");
+    config.applicationDescription.applicationUri =
+        UA_STRING("urn:open62541.variable.server");
+    config.applicationDescription.productUri =
+        UA_STRING("https://github.com/kimim/opcua-examples");
+    UA_Server *server = UA_Server_newWithConfig(&config);
+
     addVariable(server);
     UA_Server_runUntilInterrupt(server);
     UA_Server_delete(server);
--- variable_server/cpp/variable-server.cpp
+++ variable_server/cpp/variable-server.cpp
@@ -3,19 +3,26 @@
 
 int main() {
     opcua::ServerConfig config;
+    config.setApplicationName("open62541pp variable server");
+    config.setApplicationUri("urn:open62541pp.server.application");
+    config.setProductUri("https://github.com/kimim/opcua-examples");
+
     opcua::Server server{std::move(config)};
 
     // Add a variable node to the Objects node
     opcua::Node parentNode{server, opcua::ObjectId::ObjectsFolder};
     opcua::Node answerNode = parentNode.addVariable(
-        {1, "answer"}, "The Answer",
+        {1, "answer"},
+        "TheAnswer", // browse name
         opcua::VariableAttributes{}
-        .setDisplayName({"en-US", "The Answer"})
-        .setDescription(
-            {"en-US", "Answer to the Ultimate Question of Life"})
-        .setDataType<int>()
-        .setValue(opcua::Variant{42})
-    );
+            .setDisplayName({"en-US", "The Answer"})
+            .setDescription(
+                {"en-US", "Answer to the Ultimate Question of Life"})
+            .setDataType<int>()
+            .setAccessLevel(opcua::AccessLevel::CurrentRead | opcua::AccessLevel::CurrentWrite)
+            .setValue(opcua::Variant{42}),
+        opcua::VariableTypeId::BaseDataVariableType,
+        opcua::ReferenceTypeId::Organizes);
 
     server.run();
 }
