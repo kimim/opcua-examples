8d34fce3796500efc1262841d4ec583e22697a04
Kimi Ma <kimi.im@outlook.com>
2025-12-31 16:18:46 +0800

    a bit difficult to startup server

--- simple-server/clojure/src/opcua/simple_server.clj
+++ simple-server/clojure/src/opcua/simple_server.clj
@@ -3,6 +3,7 @@
   (:require [clojure.tools.logging :refer (info)])
   (:import org.eclipse.milo.opcua.sdk.server.OpcUaServer
            org.eclipse.milo.opcua.sdk.server.OpcUaServerConfig
+           org.eclipse.milo.opcua.sdk.server.EndpointConfig
            org.eclipse.milo.opcua.stack.transport.server.tcp.OpcTcpServerTransport
            org.eclipse.milo.opcua.stack.transport.server.tcp.OpcTcpServerTransportConfig
            org.eclipse.milo.opcua.stack.transport.server.OpcServerTransportFactory
@@ -11,6 +12,7 @@
 (def server
   (OpcUaServer.
    (-> (OpcUaServerConfig/builder)
+       (.setEndpoints #{(.build (EndpointConfig/newBuilder))})
        (.build))
    (reify OpcServerTransportFactory
      (create [this transport-profile]
@@ -21,4 +23,4 @@
 
 (defn -main
   [& args]
-  (.startup server))
+  (.get (.startup server)))
